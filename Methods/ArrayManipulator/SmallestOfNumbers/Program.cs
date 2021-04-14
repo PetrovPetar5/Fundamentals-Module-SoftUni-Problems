78c01d38b198b52f01bd17bec30820ee39d383e3
PetrovPetar5 <74185222+PetrovPetar5@users.noreply.github.com>
2021-04-15 00:43:23 +0300

    Add files via upload

---
+++ Methods/ArrayManipulator/SmallestOfNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace SmallestOfNumbers
+{
+    using System;
+    class Program
+    {
+        static int GivesTheSmallestNumber(int firstNumber, int secondNumber, int thirdNumber)
+        {
+            return Math.Min(firstNumber, Math.Min(secondNumber, thirdNumber));
+        }
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+            int smallestNumber = GivesTheSmallestNumber(firstNumber, secondNumber, thirdNumber);
+            Console.WriteLine(smallestNumber);
+        }
+    }
+}
---
+++ Methods/ArrayManipulator/TribonacciSequence/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace TribonacciSequence
+{
+    using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            int[] arr = { 0, 0, 1, 0 };
+            Console.Write(1 + " ");
+            for (int i = 1; i < input; i++)
+            {
+                arr[3] = arr[0] + arr[1] + arr[2];
+                Console.Write(arr[3] + " ");
+                for (int a = 0; a < arr.Length - 1; a++)
+                {
+                    arr[a] = arr[a + 1];
+                }
+            }
+        }
+    }
+}
